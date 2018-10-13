1fb4bf6ac6c97afe7fb33409a46c512bc0497dba
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-10-13 02:08:29 -0700

    moved parameters arond

--- components/networking/main.tf
+++ components/networking/main.tf
@@ -1,37 +1,42 @@
 resource "google_compute_network" "trusted_images" {
-  auto_create_subnetworks = "false"
   name                    = "${var.project_name_base}"
+
+  auto_create_subnetworks = "false"
   project                 = "${local.project_id}"
 }
 
 resource "google_compute_subnetwork" "trusted_images_us_west1" {
   name          = "${var.project_name_base}-${local.us_west1}"
+
   ip_cidr_range = "${var.subnetwork_cidr_ranges[local.us_west1]}"
+  network       = "${local.network_self_link}"
   region        = "${local.us_west1}"
   project       = "${local.project_id}"
-  network       = "${local.network_self_link}"
 }
 
 resource "google_compute_subnetwork" "trusted_images_us_central1" {
   name          = "${var.project_name_base}-${local.us_central1}"
+
   ip_cidr_range = "${var.subnetwork_cidr_ranges[local.us_central1]}"
+  network       = "${local.network_self_link}"
   region        = "${local.us_central1}"
   project       = "${local.project_id}"
-  network       = "${local.network_self_link}"
 }
 
 resource "google_compute_subnetwork" "trusted_images_us_east1" {
   name          = "${var.project_name_base}-${local.us_east1}"
+
   ip_cidr_range = "${var.subnetwork_cidr_ranges[local.us_east1]}"
+  network       = "${local.network_self_link}"
   region        = "${local.us_east1}"
   project       = "${local.project_id}"
-  network       = "${local.network_self_link}"
 }
 
 resource "google_compute_subnetwork" "trusted_images_us_east4" {
   name          = "${var.project_name_base}-${local.us_east4}"
+
   ip_cidr_range = "${var.subnetwork_cidr_ranges[local.us_east4]}"
+  network       = "${local.network_self_link}"
   region        = "${local.us_east4}"
   project       = "${local.project_id}"
-  network       = "${local.network_self_link}"
 }

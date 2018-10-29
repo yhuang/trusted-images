1b14746be9696ab68742547bf88ea56c547dade7
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-10-28 22:50:23 -0700

    removed unreferenced local variable

--- components/project/locals.tf
+++ components/project/locals.tf
@@ -1,6 +1,5 @@
 locals {
   billing_account            = "${data.google_project.terraform_admin.billing_account}"
   org_id                     = "${data.google_project.terraform_admin.org_id}"
-  terraform_admin_project_id = "${data.google_project.terraform_admin.project_id}"
   trusted_images_project_id  = "${google_project.trusted_images.project_id}"
 }
